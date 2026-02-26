385ff2eb6e6a5b9728b835e1e7ab67a69b1e4249
ben-uw <benc1213@uw.edu>
2026-02-26 13:31:46 -0800

    Add files via upload

---
+++ Assignment06.py
@@ -0,0 +1,216 @@
+# ------------------------------------------------------------------------------------------ #
+# Title: Assignment06_Starter
+# Desc: This assignment demonstrates using functions
+# with structured error handling
+# Change Log: (Who, When, What)
+#   BClemente,2/26/2030,Created Script
+# ------------------------------------------------------------------------------------------ #
+import json
+
+# Define the Data Constants
+MENU: str = """
+---- Course Registration Program ----
+  Select from the following menu:  
+    1. Register a Student for a Course.
+    2. Show current data.  
+    3. Save data to a file.
+    4. Exit the program.
+----------------------------------------- 
+"""
+
+# Define the Data Constants
+FILE_NAME: str = "Enrollments.json"
+
+# Define the Data Variables and constants
+students: list = []  # a table of student data
+menu_choice: str = ""  # Hold the choice made by the user.
+
+
+# Processing --------------------------------------- #
+class FileProcessor:
+    """
+    A collection of processing layer functions that work with JSON files
+
+    ChangeLog: (Who, When, What)
+    BClemente,2.24.2026,Created Class
+    """
+    @staticmethod
+    def read_data_from_file(file_name: str, student_data: list):
+        """
+        This function reads data from a JSON file and loads it into a list of lists.
+
+        ChangeLog: (Who, When, What)
+        BClemente,2.24.2026,Created function
+        :return: list of lists
+        """
+        file = None
+
+        try:
+            file = open(file_name, "r")
+            student_data = json.load(file)
+        except FileNotFoundError as e:
+            IO.output_error_messages("Text file must exist before running this script!", e)
+        except json.decoder.JSONDecodeError as e:
+            IO.output_error_messages("Invalid JSON file!", e)
+        except Exception as e:
+            IO.output_error_messages("There was a non-specific error!", e)
+        finally:
+            if file is not None and file.closed == False:
+                file.close()
+        return student_data
+
+    @staticmethod
+    def write_data_to_file(file_name: str, student_data: list):
+        """
+        This function writes a list of dictionary rows to a JSON file.
+
+        ChangeLog: (Who, When, What)
+        BClemente,2.24.2026,Created function
+
+        :return: None
+        """
+        try:
+            file = open(file_name, "w")
+            json.dump(student_data, file, indent=2)
+            print("The following data was saved to file!")
+        except Exception as e:
+            IO.output_error_messages("There was an error writing to the file!", e)
+        finally:
+            # Check if a file object exists and is still open
+            if file is not None and file.closed == False:
+                file.close()
+# Presentation --------------------------------------- #
+class IO:
+    """
+    A collection of presentation layer functions that manage user input and output
+
+    ChangeLog: (Who, When, What)
+    BClemente,1.1.2030,Created Class
+    """
+
+    @staticmethod
+    def output_error_messages(message: str, error: Exception = None):
+        """ This function displays a custom error messages to the user
+
+        ChangeLog: (Who, When, What)
+        BClemente,2.24.2026,Created function
+
+        :return: None
+        """
+        print(message, end="\n\n")
+        if error is not None:
+            print("-- Technical Error Message -- ")
+            print(error, error.__doc__, type(error), sep='\n')
+
+    @staticmethod
+    def output_menu(menu: str):
+        """ This function displays the menu of choices to the user
+
+        ChangeLog: (Who, When, What)
+        BClemente,2.24.2026,Created function
+
+        :return: None
+        """
+
+        print()  # Adding extra space to make it look nicer.
+        print(menu)
+        print()  # Adding extra space to make it look nicer.
+
+    @staticmethod
+    def input_menu_choice():
+        """ This function gets a menu choice from the user
+
+        ChangeLog: (Who, When, What)
+        BClemente,2.24.2026,Created function
+
+        :return: string with the users choice
+        """
+        choice = "0"
+        try:
+            choice = input("Enter your menu choice number: ")
+            if choice not in ("1","2","3","4"):
+                raise Exception("Please, choose only 1, 2, 3, or 4")
+        except Exception as e:
+            IO.output_error_messages(e.__str__())  # Not passing the exception object to avoid the technical message
+
+        return choice
+
+    @staticmethod
+    def input_student_data(student_data: list):
+        """ This function gets the student's first name, last name, and course name from the user.
+
+        ChangeLog: (Who, When, What)
+        BClemente,2.24.2026,Created function
+
+        :return: list of dictionaries
+        """
+        try:
+            student_first_name = input("Enter the student's first name: ")
+            if not student_first_name.isalpha():
+                raise ValueError("The first name should not contain numbers.")
+
+            student_last_name = input("Enter the student's last name: ")
+            if not student_last_name.isalpha():
+                raise ValueError("The last name should not contain numbers.")
+
+            course_name = input("Please enter the name of the course: ")
+            students = {"FirstName": student_first_name,
+                            "LastName": student_last_name,
+                            "CourseName": course_name}
+
+            student_data.append(students)
+            print(f"Registered {student_first_name} {student_last_name} for {course_name}.")
+        except ValueError as e:
+            IO.output_error_messages("The value is not the correct type of data!", e)
+        except Exception as e:
+            IO.output_error_messages("There was a non-specific error!", e)
+        return student_data
+
+    @staticmethod
+    def output_student_courses(student_data: list):
+        """ This function returns student data in a string to the user
+
+        ChangeLog: (Who, When, What)
+        BClemente,2.24.2026,Created function
+
+        :return: None
+        """
+        print("-" * 50)
+        for student in student_data:
+            print(f"{student["FirstName"]},{student["LastName"]},{student["CourseName"]}")
+        print("-" * 50)
+
+# When the program starts, read the file data into a list of lists (table)
+
+# Extract the data from the file
+students = FileProcessor.read_data_from_file(file_name=FILE_NAME, student_data=students)
+
+# Present and Process the data
+while True:
+
+    # Present the menu of choices
+    IO.output_menu(menu=MENU)
+
+    menu_choice = IO.input_menu_choice()
+
+    # Input user data
+    if menu_choice == "1":  # This will not work if it is an integer!
+        students = IO.input_student_data(student_data=students)
+        continue
+
+    # Present the current data
+    elif menu_choice == "2":
+        IO.output_student_courses(student_data=students)
+        continue
+
+    # Save the data to a file
+    elif menu_choice == "3":
+        FileProcessor.write_data_to_file(file_name=FILE_NAME, student_data=students)
+        IO.output_student_courses(student_data=students)
+        continue
+
+    # Stop the loop
+    elif menu_choice == "4":
+        break  # out of the loop
+
+print("Program Ended")
